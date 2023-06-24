8834b1bc632bcf218334cf251697fad2c606901e
Desmond51 <desmondtekum9@gmail.com>
2023-06-24 19:57:52 +0100

    CV V1 completed

--- src/ContentPage.js
+++ src/ContentPage.js
@@ -1,42 +1,49 @@
-import { Divider, List, Typography } from '@mui/material'
+import { Box, Divider, Grid, List, Typography } from '@mui/material'
 // import {Divider} from '@mui/material/joy/Divider'
 import React from 'react'
 
 export default function ContentPage() {
   return (
-    <div>
+    <container>
+      <Typography  marginY={3} bgcolor={'#1b033E'} width={'100%'}>..</Typography>
       <div>
-      <Typography > Tekum Desmond Teko</Typography>
-      <Typography>Software Developer</Typography>
+      <Typography fontSize={30} align='left' > Tekum Desmond Teko</Typography>
+      <Typography fontSize={16} align='left' marginBottom={3}>Software Developer</Typography>
        <Divider
-       sx={{width:200}}
+       sx={{width:'100%'}}
        />
       </div>
       <div >
       <Typography
-      marginTop={5}
-      fontSize={30}
-      bgcolor={'#f5f0EA'}
+        paddingLeft={2}
+        width={570}
+        marginTop={5}
+        fontSize={25}
+        bgcolor={'#f5f0EA'}
       ><span class="e896 md-light md-lg"></span> ABOUT ME</Typography>
       <Typography
-      marginTop={3}
+         fontSize={13}
+         width={370}
+         marginTop={3}
       >
       I am a software engineer with 2 years of experience in the industry. I am passionate about building innovative and user-friendly software that solves real-world problems. I have a strong understanding of the software development process, from requirements gathering to testing and deployment. I am also proficient in a variety of programming languages and technologies, including Python, React, and Node.js.
       </Typography>
       </div>
       <div >
       <Typography
-      marginTop={5}
-      fontSize={30}
-      bgcolor={'#f5f0EA'}
+        paddingLeft={2}
+        width={570}
+        marginTop={5}
+        fontSize={25}
+        bgcolor={'#f5f0EA'}
       ><span class="e896 md-light md-lg"></span> EXPERIENCE</Typography>
       <Typography
       marginTop={3}
       >
-           <Typography fontSize={14} color={'whitesmoke'} bgcolor={'#1b033E'} width={170}>May 2022 - Present</Typography>
-            <Typography>
+           <Typography fontSize={13} color={'whitesmoke'} marginBottom={1} bgcolor={'#1b033E'} width={170}>May 2022 - Present</Typography>
+            <Typography width={370} fontSize={13}>
             In my current roles, I'm working on a variety of projects, including: <br />
-               <Typography marginLeft={3}>
+               <Typography marginLeft={3} fontSize={13}>
                  •Developing a new e-commerce platform for a major retailer. <br />
                  •Building a mobile app for a financial services company. <br />
                  •Creating a web-based application for a government agency
@@ -45,33 +52,40 @@ export default function ContentPage() {
             </Typography>
       </Typography>
       </div>
-      <div >
+      <Grid container  sx={{ flexGrow: 1 }}>
+      <Grid sx={{marginRight:4}} >
       <Typography
-      marginTop={5}
-      fontSize={30}
-      bgcolor={'#f5f0EA'}
+         paddingLeft={2}
+         width={230}
+         marginTop={5}
+         fontSize={25}
+         bgcolor={'#f5f0EA'}
       ><span class="e896 md-light md-lg"></span>LANGUAGE</Typography>
       <Typography
-      marginTop={3}
+         marginTop={3}
       >
-        <span>
-        <Typography>English</Typography>
-        <Typography fontSize={12} color={'gray'}>Native</Typography></span>
+        <Typography fontSize={14}>English</Typography>
+        <Typography fontSize={13} color={'gray'}>Native</Typography>
       
       </Typography>
-      </div>
-      <div >
+      </Grid>
+      <Grid >
       <Typography
-      marginTop={5}
-      fontSize={30}
-      bgcolor={'#f5f0EA'}
-      ><span class="e896 md-light md-lg"></span> ABOUT ME</Typography>
+         paddingLeft={2}
+         width={230}
+         marginTop={5}
+         fontSize={25}
+         bgcolor={'#f5f0EA'}
+      ><span class="e896 md-light md-lg"></span> REFERENCE</Typography>
       <Typography
-      marginTop={3}
+         marginTop={3}
       >
-      I am a software engineer with 2 years of experience in the industry. I am passionate about building innovative and user-friendly software that solves real-world problems. I have a strong understanding of the software development process, from requirements gathering to testing and deployment. I am also proficient in a variety of programming languages and technologies, including Python, React, and Node.js.
+        <Typography fontSize={14}>General manager of Iknite space</Typography>
+        <Typography fontSize={13} color={'gray'}>+23768699673</Typography>
       </Typography>
-      </div>
-    </div>
+      </Grid>
+      </Grid>
+      <Typography marginTop={4} bgcolor={'#1b033E'} width={'100%'}>..</Typography>
+    </container>
   )
 }
--- src/SideBar.js
+++ src/SideBar.js
@@ -1,36 +1,90 @@
-import { AddBox, Person } from '@mui/icons-material';
 import { Avatar, Box, Container, Grid, Typography } from '@mui/material';
-import { deepOrange } from '@mui/material/colors';
 import * as React from 'react';
+import CallOutlinedIcon from '@mui/icons-material/CallOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import RoomOutlinedIcon from '@mui/icons-material/RoomOutlined';
+import RemoveFromQueueIcon from '@mui/icons-material/RemoveFromQueue';
 
 
 export default function SideBar() {
 
   return (
-     <Container>
+     <Container sx={{}}>
       <div>
-      <Avatar sx={{ bgcolor: deepOrange[500], padding:6, marginLeft:5 }}
-  alt="Remy Sharp" />
-      <Typography
-      marginTop={2}
-        fontSize={18}
-        align='center'
-        color={''}
-      >TEKUM DESMOND TEKO</Typography>
+      <Avatar sx={{ bgcolor: '#f8f0EA', padding:6, marginLeft:5 }}
+       alt="Desmond Teko" src="./public/me.jpeg" />
+  
       </div>
-
       <div>
-      <Typography 
-      fontSize={23}
-      marginTop={10}
-      marginLeft={9}
+      <Grid >
+      <Typography
+         paddingLeft={2}
+         width={230}
+         marginTop={7}
+         fontSize={25}
+         bgcolor={'#f5f0EA'}
+      ><span class="e896 md-light md-lg"></span> CONTACT</Typography>
+      <Typography
+         marginTop={3}
       >
-        Details
+        <Typography fontSize={13}><EmailOutlinedIcon fontSize='30' marginTop={4} />desmondtekum9@gmail.com</Typography>
+        <Typography fontSize={13} ><CallOutlinedIcon fontSize='30' />+23768699673</Typography>
+        <Typography fontSize={13} ><RoomOutlinedIcon  fontSize='30' />Molyko, Buea, Cameroon</Typography>
+      </Typography>
+      </Grid>
+
+      <Typography bgcolor={'#f5f0EA'} paddingLeft={2}>
+ 
+      <Typography >
+      <Typography
+         width={230}
+         marginTop={7}
+         fontSize={25}
+         bgcolor={'#f5f0EA'}
+      ><span class="e896 md-light md-lg"></span> Education</Typography>
+      <Typography
+         marginTop={3}
+      >
+      <Typography
+         marginTop={3}
+      >
+        <Typography fontSize={14}>Advanced Level</Typography>
+        <Typography fontSize={13} color={'gray'}>Baptist High School Buea</Typography>
+      </Typography>
+      <Typography
+         marginTop={1}
+      >
+        <Typography fontSize={14}>Barchelor's Degree</Typography>
+        <Typography fontSize={13} color={'gray'}>Saint Monica University Higher Institute of Buea</Typography>
+      </Typography>
+
+      </Typography>
+      </Typography>
+
+      <Typography >
+      <Typography
+         width={230}
+         marginTop={7}
+         fontSize={25}
+         bgcolor={'#f5f0EA'}
+      ><span class="e896 md-light md-lg"></span> SKILLS</Typography>
+      <Typography
+         marginTop={3}
+      >
+        <Typography fontSize={13}><RemoveFromQueueIcon fontSize='30' marginTop={4} />JavaScript</Typography>
+        <Typography fontSize={13} ><RemoveFromQueueIcon fontSize='30' />TypeScript</Typography>
+        <Typography fontSize={13} ><RemoveFromQueueIcon fontSize='30' />React</Typography>
+        <Typography fontSize={13}><RemoveFromQueueIcon fontSize='30' marginTop={4} />MongoDB</Typography>
+        <Typography fontSize={13} ><RemoveFromQueueIcon fontSize='30' />Node.js</Typography>
+        <Typography fontSize={13} ><RemoveFromQueueIcon fontSize='30' />Express</Typography>
+        <Typography fontSize={13}><RemoveFromQueueIcon fontSize='30' marginTop={4} />Next.js</Typography>
+        <Typography fontSize={13} ><RemoveFromQueueIcon fontSize='30' />Tailwind</Typography>
+        <Typography fontSize={13} ><RemoveFromQueueIcon fontSize='30' />Bootstrap</Typography>
+        <Typography fontSize={13} ><RemoveFromQueueIcon fontSize='30' />Material UI</Typography>
+      </Typography>
       </Typography>
        
-        {/* <Typography  startDecorator={<Person />}>
-            Hello World
-          </Typography> */}
+      </Typography>
        
       </div>
 
